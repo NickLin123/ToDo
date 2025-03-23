1bdfe0a655ce28b837653b567a58dd6c7cf6134e
Nick <s0913741399@gmail.com>
2025-03-23 17:54:20 +0800

    新增tailwinds、輸入框不得為空值、清單最多10筆

--- ToDo/src/components/TodoList.vue
+++ ToDo/src/components/TodoList.vue
@@ -2,6 +2,7 @@
 import { ref,onMounted } from 'vue'
 
 let id =0;
+// let Edit = true;
 const NewItem = ref('')
 const ItemList=  ref([])
 const STORAGE_KEY = 'vue-todolist-items';
@@ -9,10 +10,19 @@ onMounted(() => {
   loadTodos();
 });
 
+
 const AddItem =()=>{
+  if (NewItem.value.trim()=='' || ItemList.value.length>=10) {
+      return; // 
+    }
   ItemList.value.push({ id: id++, text: NewItem.value })
   NewItem.value = ''
   saveTodos()
+}
+const EditItem=(Edit)=> {
+  Edit !== Edit
+  console.log(Edit)
+  // saveTodos()
 }
 const removeItem=(item)=> {
   ItemList.value = ItemList.value.filter((t) => t !== item)
@@ -31,17 +41,27 @@ const saveTodos = () => {
 </script>
 
 <template>
-    <div class="card">
-      <form @submit.prevent="AddItem">
-        <input v-model="NewItem" placeholder="Type here">
-        <button type="button" @click="AddItem">新增</button>
+    <div class="card m-4 p-3 shadow-md rounded-md">
+      <form class="m-1 p-1" @submit.prevent="AddItem">
+        <input class="m-4 p-3 border-4 border-indigo-500 rounded-md"  v-model="NewItem" placeholder="待辦事項">
+        <button class="m-2 p-3 bg-blue-600 text-neutral-100" type="button" @click="AddItem">新增</button>
       </form>
       <ul>
-          <li v-for="item in ItemList" :key="item.id">{{ item.text }}
-          <button @click="removeItem(item)">刪除</button>
+          <li class="m-1 p-1" v-for="item in ItemList" :key="item.id">{{ item.text }}
+          <button class="m-2 p-3 bg-blue-400 text-neutral-100" @click="EditItemItem(item)">編輯</button>
+          <button class="m-2 p-3 bg-pink-300 text-neutral-100" @click="removeItem(item)">刪除</button>
         </li>
       </ul>  
     </div>
+
+    <button class="m-2 p-3 bg-blue-400 text-neutral-100" @click="EditItem(Edit)">編輯</button>
+    <div v-if="Edit==true">
+         Now you see me
+    </div>
+    <div v-else>
+      Now you don't
+    </div>
+  
 </template>
 
 <style scoped>
